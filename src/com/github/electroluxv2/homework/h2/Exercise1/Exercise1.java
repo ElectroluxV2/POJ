3589b1f6f8b1832b1e02eb05a15bbfd41d7d4e55
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-18 14:19:44 +0100

    Exercise 1

---
+++ src/com/github/electroluxv2/homework/h2/Exercise1/Exercise1.java
@@ -0,0 +1,20 @@
+package com.github.electroluxv2.homework.h2.Exercise1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter length of array and its elements: ");
+        int length = scanner.nextInt();
+        char[] array = new char[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = scanner.next().charAt(0);
+        }
+
+        System.out.println("array = " + Arrays.toString(array));
+    }
+}
---
+++ src/com/github/electroluxv2/homework/h2/Exercise1/README.md
@@ -0,0 +1,6 @@
+# Exercise 3
+Napisz program, który pobiera od użytkownika wielkość tablicy, a następnie
+uzupełnia ją wpisywanymi przez użytkownika znakami. Na koniec program wyświetla utworzona
+przez użytkownika tablicę
+# Result
+![Result](./img.png?raw=true)
