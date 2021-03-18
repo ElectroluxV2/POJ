f2de8c7302f9b2d6d1b532cee3248fe5a45b447c
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-18 14:24:14 +0100

    Exercise 2

---
+++ src/com/github/electroluxv2/homework/h2/Exercise2/Exercise2.java
@@ -0,0 +1,26 @@
+package com.github.electroluxv2.homework.h2.Exercise2;
+
+import java.util.Arrays;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+
+        int[] array1 = new int[] { 1, 2, 3 };
+        int[] array2 = new int[] { 4, 5, 6 };
+
+        System.out.println("array = " + Arrays.toString(SumArrays(array1, array2)));
+    }
+
+    private static int[] SumArrays(int[] array1, int[] array2) {
+        if (array1.length != array2.length) return new int[0];
+
+        int[] result = new int[array1.length];
+
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i] + array2[i];
+        }
+
+        return result;
+    }
+}
---
+++ src/com/github/electroluxv2/homework/h2/Exercise2/README.md
@@ -0,0 +1,6 @@
+# Exercise 2
+Napisz metodę, która przyjmuje 2 tablice 1wym. o tej samej długości i zwraca
+nową tablicę stanowiącą sumę przekazanych tablic. W sytuacji gdy długości tablic się różnią metoda
+powinna zwrócić pustą tablicę.
+# Result
+![Result](./img.png?raw=true)
