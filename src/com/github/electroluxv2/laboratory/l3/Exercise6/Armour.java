b9c059ae520de0f96c6e2901571242d5faae57d3
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-26 00:53:55 +0100

    Exercise 6

---
+++ src/com/github/electroluxv2/laboratory/l3/Exercise6/Armour.java
@@ -0,0 +1,37 @@
+package com.github.electroluxv2.laboratory.l3.Exercise6;
+
+import java.util.Random;
+
+enum Armour {
+    CALTROPS(0, 2),
+    SHIELD(6, 8),
+    IRON_MAIDEN(0, 20);
+
+    private final int minimumDefencePoints;
+    private final int maximumDefencePoints;
+
+    Armour(int minimumDefencePoints, int maximumDefencePoints) {
+        this.minimumDefencePoints = minimumDefencePoints;
+        this.maximumDefencePoints = maximumDefencePoints;
+    }
+
+    public static Armour randomChoose(Random random) {
+        return Armour.values()[random.nextInt(Armour.values().length)];
+    }
+
+    public int getMinimumDefencePoints() {
+        return minimumDefencePoints;
+    }
+
+    public int getMaximumDefencePoints() {
+        return maximumDefencePoints;
+    }
+
+    public int getDefencePoints(Random random) {
+        return random.nextInt(this.maximumDefencePoints + 1 - this.minimumDefencePoints) + this.minimumDefencePoints;
+    }
+
+    public int getWeight() {
+        return minimumDefencePoints + maximumDefencePoints + 1;
+    }
+}
---
+++ src/com/github/electroluxv2/laboratory/l3/Exercise6/README.md
@@ -0,0 +1,18 @@
+# Exercise 6
+Stwórz symulator walki – stwórz klasę Player. Player ma punkty życia, broń oraz zbroję.<br>
+Program powinien stworzyć 10 random-owych przeciwników (player-ów).<br><br>
+
+Niech użytkownik (nowy Player) może wybrać jedną z dostępnych broni / zbroi, a następnie
+może wybrać przeciwnika, z którym walczy.<br><br>
+
+Walka toczy się cios za cios, najpierw gracz, potem przeciwnik i tak aż do śmierci jednego z
+nich. Postarać się zrobić tak, by każdy z nich miał szansę wygrać (niekoniecznie równą, np.
+podyktowaną wyborem ekwipunku). Można dodać coś od siebie jak ktoś ma wenę.
+
+# Result
+![Result](./img.png?raw=true)
+![Result](./img_1.png?raw=true)
+![Result](./img_2.png?raw=true)
+![Result](./img_3.png?raw=true)
+![Result](./img_4.png?raw=true)
+![Result](./img_5.png?raw=true)
