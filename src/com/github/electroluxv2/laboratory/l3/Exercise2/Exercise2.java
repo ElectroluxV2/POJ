35c810f6bf91da881fa38719f2807dc8307635f5
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-25 13:03:42 +0100

    Exercise 2

---
+++ src/com/github/electroluxv2/laboratory/l3/Exercise2/Exercise2.java
@@ -0,0 +1,29 @@
+package com.github.electroluxv2.laboratory.l3.Exercise2;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Point first = new Point();
+        Point second = new Point(4444.32423, 234.1234432445);
+
+        System.out.printf("Distance between points is: %f%n", first.DistanceTo(second));
+    }
+
+    private static class Point {
+        double x = 0, y = 0;
+
+        public Point() {}
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double DistanceTo(double x, double y) {
+            return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+        }
+
+        public double DistanceTo(Point other) {
+            return this.DistanceTo(other.x, other.y);
+        }
+    }
+}
---
+++ src/com/github/electroluxv2/laboratory/l3/Exercise2/README.md
@@ -0,0 +1,5 @@
+# Exercise 2
+Stwórz klasę Punkt która reprezentować będzie punkt w przestrzeni x i y. Ma posiadać:• pola x i y• konstruktor bezargumentowy tworzący punkt w koordynatach (0,0)• konstruktor pozwalający stworzyć punkt o określonych koordynatach• metodę pozwalającą na obliczenie odległości między tym punktem a innym punktempodanym jako x i y• metodę pozwalającą na obliczenie odległości między tym punktem a innym punktempodanym jako obiekt typu Punkt
+
+# Result
+![Result](./img.png?raw=true)
