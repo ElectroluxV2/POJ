87645b2024ce597f2dcf2366ba1a22beeef507e1
ElectroluxV2 <electroluxv2@gmail.com>
2021-04-08 23:10:00 +0200

    Added final in Main.main

--- src/com/github/electroluxv2/laboratory/l5/Exercise1/Main.java
+++ src/com/github/electroluxv2/laboratory/l5/Exercise1/Main.java
@@ -3,8 +3,8 @@ package com.github.electroluxv2.laboratory.l5.Exercise1;
 public class Main {
 
     public static void main(String[] args) {
-        Cat cat = new Cat("Robert", "Black");
-        Dog dog = new Dog("Artur", "White");
+        final Cat cat = new Cat("Robert", "Black");
+        final Dog dog = new Dog("Artur", "White");
 
         System.out.println(cat.makeSound());
         System.out.println(cat);
