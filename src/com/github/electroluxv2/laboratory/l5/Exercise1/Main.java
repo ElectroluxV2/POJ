65c06af99d5bc1f9aacc132a62dc8b5d9b999c43
ElectroluxV2 <electroluxv2@gmail.com>
2021-04-08 13:20:15 +0200

    Java 16

--- src/com/github/electroluxv2/laboratory/l5/Exercise1/Main.java
+++ src/com/github/electroluxv2/laboratory/l5/Exercise1/Main.java
@@ -3,6 +3,12 @@ package com.github.electroluxv2.laboratory.l5.Exercise1;
 public class Main {
 
     public static void main(String[] args) {
+        Cat cat = new Cat("Robert", "Black");
+        Dog dog = new Dog("Artur", "White");
 
+        System.out.println(cat.makeSound());
+        System.out.println(cat);
+        System.out.println(dog.makeSound());
+        System.out.println(dog);
     }
 }
