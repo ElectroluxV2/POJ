2f09247e11051bedabe2edba76316a4dc280310a
ElectroluxV2 <electroluxv2@gmail.com>
2021-04-09 01:21:51 +0200

    Exercise 4

---
+++ src/com/github/electroluxv2/laboratory/l5/Exercise4/Class.java
@@ -0,0 +1,22 @@
+package com.github.electroluxv2.laboratory.l5.Exercise4;
+
+import java.util.Arrays;
+
+class Class {
+    private final String[] args;
+
+    public Class(String arg1, String arg2, String arg3, String arg4, String arg5) {
+        this.args = new String[] {
+            arg1,
+            arg2,
+            arg3,
+            arg4,
+            arg5
+        };
+    }
+
+    @Override
+    public String toString() {
+        return "Class containing args: %s".formatted(Arrays.toString(this.args));
+    }
+}
--- src/com/github/electroluxv2/laboratory/l5/Exercise4/Main.java
+++ src/com/github/electroluxv2/laboratory/l5/Exercise4/Main.java
@@ -3,6 +3,7 @@ package com.github.electroluxv2.laboratory.l5.Exercise4;
 class Main {
 
     public static void main(String[] args) {
-      
+        Class classIsProhibitedWordButTheContentOfTheTaskClearlySpecifyClassAsNameLOL = new Class("a", "b", "c", "d", "e");
+        System.out.println(classIsProhibitedWordButTheContentOfTheTaskClearlySpecifyClassAsNameLOL);
     }
 }
