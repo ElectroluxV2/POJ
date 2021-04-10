45acf28043feab4e5dc432febe4cf2f59a292f57
ElectroluxV2 <electroluxv2@gmail.com>
2021-04-10 21:53:30 +0200

    Varargs

--- src/com/github/electroluxv2/laboratory/l5/exercise4/Class.java
+++ src/com/github/electroluxv2/laboratory/l5/exercise4/Class.java
@@ -5,14 +5,8 @@ import java.util.Arrays;
 final class Class {
     private final String[] args;
 
-    public Class(final String arg1, final String arg2, final String arg3, final String arg4, final String arg5) {
-        this.args = new String[] {
-            arg1,
-            arg2,
-            arg3,
-            arg4,
-            arg5
-        };
+    public Class(final String... args) {
+        this.args = args;
     }
 
     @Override
