1ebbdf5c0d8cf47f44058819b5375bc8c968819d
ElectroluxV2 <electroluxv2@gmail.com>
2021-05-06 14:36:54 +0200

    Exercise 1

--- src/com/github/electroluxv2/laboratory/l9/exercise1/Main.java
+++ src/com/github/electroluxv2/laboratory/l9/exercise1/Main.java
@@ -3,6 +3,24 @@ package com.github.electroluxv2.laboratory.l9.exercise1;
 public class Main {
 
     public static void main(final String[] args) {
+        MyList<Integer> integers = new MyList<>();
 
+        integers.add(1);
+        integers.add(2);
+        integers.add(1);
+
+        System.out.printf("First: %d, Last: %d%n", integers.indexOf(1), integers.lastIndexOf(1));
+
+        MyList<String> strings = new MyList<>();
+
+        strings.add("aaa");
+
+        if (strings.contains("aaa")) {
+            System.out.println("Contains aaa");
+        }
+
+        if (!strings.contains("bbb")) {
+            System.out.println("Not contains bbb");
+        }
     }
 }
---
+++ src/com/github/electroluxv2/laboratory/l9/exercise1/MyList.java
@@ -0,0 +1,68 @@
+package com.github.electroluxv2.laboratory.l9.exercise1;
+
+public class MyList<T> {
+    private int currentIndex = -1;
+    private final T[] array;
+
+    @SuppressWarnings({"unchecked"})
+    public MyList() {
+        this.array = (T[]) new Object[20];
+    }
+
+    public boolean add(final T element) {
+        if (++this.currentIndex >= 20) { return false; }
+        this.array[this.currentIndex] = element;
+        return true;
+    }
+
+    public int size() {
+        return this.currentIndex;
+    }
+
+    public boolean contains(final T element) {
+        for (int i = 0; i < this.currentIndex + 1; i++) {
+            final T item = this.array[i];
+            if (item.equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(final T element) {
+        for (int i = 0; i < this.currentIndex; i++) {
+            final T item = this.array[i];
+
+            if (item.equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(final T element) {
+        for (int i = this.currentIndex; i > 0; i--) {
+            final T item = this.array[i];
+            if (item.equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public T get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= this.currentIndex) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return this.array[index];
+    }
+
+    public void set(int index, T value) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= 20) {
+            throw new IndexOutOfBoundsException();
+        }
+
+       this.array[index] = value;
+    }
+}
