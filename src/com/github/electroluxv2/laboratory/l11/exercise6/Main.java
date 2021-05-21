b2f9cc9c796bc38b2224863c7f3d56837128eef0
ElectroluxV2 <electroluxv2@gmail.com>
2021-05-21 20:09:54 +0200

    Exercise 6 part 3

--- src/com/github/electroluxv2/laboratory/l11/exercise6/Main.java
+++ src/com/github/electroluxv2/laboratory/l11/exercise6/Main.java
@@ -12,55 +12,56 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
     static final JFrame frame = new JFrame("Exercise 6");
-    static final JLabel informationLabel = new JLabel("Select text file to calculate results. File > Open", SwingConstants.CENTER);
-    static final JLabel bottomLabel = new JLabel();
+    static final JLabel bottomLabel = new JLabel("Select text file to calculate results. File > Open", SwingConstants.CENTER);
     static final JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-    static JScrollPane scrollPane;
-    static JPanel panel = new JPanel();
+    static JPanel labelPanel = new JPanel();
 
     public static void setUpFrame() {
-        final Toolkit toolkit = Toolkit.getDefaultToolkit();
-        final Dimension screenSize = toolkit.getScreenSize();
-        frame.setLocation(0, 0);
-        frame.setSize(screenSize.width, screenSize.height);
-        frame.setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        // Main panel
+        final JPanel panel = new JPanel(new BorderLayout());
 
+        // Scroll pane with dynamic labels
+        labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.PAGE_AXIS));
+        final JScrollPane scrollPane = new JScrollPane(labelPanel);
+
+        // File chooser
+        fileChooser.setFileFilter(new FileNameExtensionFilter("text files", "txt", "text"));
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.addActionListener(Main::onFileSelected);
+
+        // Bottom Label
+        bottomLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        // Menu
         final JMenuBar menuBar = new JMenuBar();
         final JMenu menu = new JMenu("File");
         final JMenuItem item = new JMenuItem("Open");
         item.addActionListener(Main::onMenuItemClick);
         menu.add(item);
         menuBar.add(menu);
-        frame.setJMenuBar(menuBar);
-
-        //final JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        scrollPane = new JScrollPane(panel);
-        frame.setContentPane(scrollPane);
-        //panel.add(scrollPane, BorderLayout.CENTER);
-        // panel.add(informationLabel, SwingConstants.CENTER);
 
 
-        fileChooser.setFileFilter(new FileNameExtensionFilter("text files", "txt", "text"));
-        fileChooser.setAcceptAllFileFilterUsed(false);
-        fileChooser.addActionListener(Main::onFileSelected);
-
-
-        bottomLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        // Add Components
+        panel.add(scrollPane, BorderLayout.CENTER);
         panel.add(bottomLabel, BorderLayout.SOUTH);
 
-
-        //frame.setContentPane(panel);
+        // Main frame
+        final Toolkit toolkit = Toolkit.getDefaultToolkit();
+        final Dimension screenSize = toolkit.getScreenSize();
+        frame.setLocation(0, 0);
+        frame.setSize(screenSize.width, screenSize.height);
+        frame.setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        frame.setJMenuBar(menuBar);
+        frame.setContentPane(panel);
         frame.setVisible(true);
     }
 
     public static void onMenuItemClick(final ActionEvent event) {
-        // No nne to check what item as there is only one item
+        // No need to check what item as there is only one item
         fileChooser.showOpenDialog(frame);
     }
 
@@ -73,16 +74,22 @@ public class Main {
 
     public static void readFileLineByLine(final File file) {
         try {
+
+            // Clear last result
+            labelPanel.removeAll();
+
+            // Make label of every line in file
             final List<String> contents = Files.lines(file.toPath()).toList();
             for (int i = 0; i < contents.size(); i++) {
                 final String line = contents.get(i);
                 final JLabel label = new JLabel("L%d: %s".formatted(i, line));
                 System.out.println(line);
-                panel.add(label);
+                labelPanel.add(label);
             }
 
-            scrollPane.revalidate();
-            scrollPane.repaint();
+            // Show new labels in scroll pane
+            labelPanel.revalidate();
+            labelPanel.repaint();
 
         } catch (IOException e) {
             bottomLabel.setText("An error occurred during file read: %s".formatted(e.getMessage()));
